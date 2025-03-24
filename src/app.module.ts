dbc2d629e615d57b584a37c87aad4f463be50964
hazemr2121 <ha.ahmed8996@gmail.com>
2025-03-24 15:27:36 +0200

    folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeacherModule } from './teacher/teacher.module';
+import { SchoolModule } from './school/school.module';
 
 @Module({
-  imports: [],
+  imports: [TeacherModule, SchoolModule],
   controllers: [AppController],
   providers: [AppService],
 })
